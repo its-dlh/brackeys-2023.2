be6cff7a78ee4938299bfe7878d38008a9f87b24
Dawson Goodell <dawsong@osmstudios.com>
2023-08-24 21:01:37 -0700

    Jointless anchoring

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,58 +1,79 @@
-extends Node2D
+extends RigidBody2D
 
-export var max_throw_distance = 300
-export var throw_speed = 400
-export var anchor_slack = 10
+enum PlayerStates {DEFAULT, HOOKED}
+enum HookStates {NONE, EXTEND, RETRACT_TO_PLAYER}
 
-onready var rigid_body = $RigidBody
-onready var anchor = $Anchor
+export var target_movement_time = 0.2
+export var target_accel_time = 0.1
+export var max_torque: float = 1000
 
-var hook_state = HookStates.idle
-var is_pinned = false
-var anchor_pin: PinJoint2D = null
+onready var hook = $Anchor
+onready var hook_shape := $Anchor/CollisionShape2D
 
-enum HookStates {
-	idle,
-	extended,
-	stuck
-}
+const HOOK_MAX_LENGTH = 600.0
+const HOOK_SPEED = 800.0
+const PLAYER_HOOK_SPEED = 600.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
+var hook_direction: Vector2
+var player_state = PlayerStates.DEFAULT
+var hook_state = HookStates.NONE
 
 func _physics_process(delta):
-	if anchor.position.distance_to(rigid_body.position) > max_throw_distance and hook_state != HookStates.stuck and not is_pinned:
-		is_pinned = true
-		anchor_pin = PinJoint2D.new()
-		anchor_pin.position = rigid_body.position
-		anchor_pin.node_a = rigid_body.get_path()
-		anchor_pin.node_b = anchor.get_path()
+	look_follow(global_position, get_global_mouse_position())
+	
+	match player_state:
+		PlayerStates.DEFAULT:
+			pass
+		PlayerStates.HOOKED:
+			var collision = get_colliding_bodies()
+			if not collision.empty():
+				player_state = PlayerStates.DEFAULT
+				hide_hook()
+			elif global_position.distance_to(hook.global_position) <= PLAYER_HOOK_SPEED * delta:
+				global_position = hook.global_position
+				player_state = PlayerStates.DEFAULT
+				hide_hook()
+			if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
+				player_state = PlayerStates.DEFAULT
+				hook_state = HookStates.RETRACT_TO_PLAYER
+			if Input.is_action_pressed("ui_up"):
+				player_state = PlayerStates.DEFAULT
+				hook_state = HookStates.RETRACT_TO_PLAYER
+	match hook_state:
+		HookStates.EXTEND:
+			hook.global_position += hook_direction * HOOK_SPEED * delta
+			if hook.global_position.distance_to(global_position) >= HOOK_MAX_LENGTH:
+				hook_state = HookStates.RETRACT_TO_PLAYER
+				hook_shape.call_deferred("set_disabled", true)
+		HookStates.RETRACT_TO_PLAYER:
+			hook.global_position += hook.global_position.direction_to(global_position) * HOOK_SPEED * delta
+			if hook.global_position.distance_to(global_position) <= HOOK_SPEED * delta:
+				hook_state = HookStates.NONE
+				hide_hook()
 
-		add_child(anchor_pin)
+func _on_Anchor_body_entered(_body):
+	player_state = PlayerStates.HOOKED
+	apply_impulse(Vector2.ZERO, global_position.direction_to(hook.global_position) * PLAYER_HOOK_SPEED)
+	hook_state = HookStates.NONE
+	hook_shape.call_deferred("set_disabled", true)
 
-func _input(event):
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
-		if hook_state == HookStates.idle:
-			hook_state = HookStates.extended
-			var impulse_vector = Vector2(throw_speed, 0).rotated(rigid_body.rotation)
-			anchor.apply_impulse(Vector2(), impulse_vector)
+func look_follow(current_position, target_position):
+	var target_angle = atan2(target_position.y - current_position.y, target_position.x - current_position.x)
+	var current_angle = rotation
+	var difference = target_angle - current_angle
 
+	if difference > PI:
+		difference -= PI * 2
+	elif difference < -PI:
+		difference += PI * 2
 
-func _on_Anchor_body_entered(body):
-	if (hook_state == HookStates.extended):
-		hook_state = HookStates.stuck
-		anchor.sleeping = true
-		var chain = DampedSpringJoint2D.new()
-		chain.stiffness = 0
-		chain.damping = 0
-		chain.position = rigid_body.position
-		chain.rest_length = rigid_body.position.distance_to(body.position)
-		chain.length = chain.rest_length + anchor_slack
-		chain.rotation = rad2deg(rigid_body.position.angle_to(body.position))
-		chain.node_a = rigid_body.get_path()
-		chain.node_b = body.get_path()
+	attempt_angular_velocity(difference / target_movement_time)
 
-		add_child(chain)
+func attempt_angular_velocity(target_velocity: float):
+	var target_acceleration = (target_velocity - angular_velocity) / target_accel_time
+	var target_torque = inertia * target_acceleration
+	add_torque(clamp(target_torque, -max_torque, max_torque))
 
-
+func hide_hook():
+	hook.hide()
+	hook_shape.call_deferred("set_disabled", true)
