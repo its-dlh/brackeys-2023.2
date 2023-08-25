625dc06b2f9e9e587658574ca2cce4a5b034a030
Dawson Goodell <dawsong@osmstudios.com>
2023-08-24 22:29:18 -0700

    Improved hook throwing
    Buggy as hell, but hey

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 enum PlayerStates {DEFAULT, HOOKED}
 enum HookStates {NONE, EXTEND, RETRACT_TO_PLAYER}
 
+export var throw_speed = 0.5
 export var target_movement_time = 0.2
 export var target_accel_time = 0.1
 export var max_torque: float = 1000
@@ -10,7 +11,7 @@ export var max_torque: float = 1000
 onready var hook = $Anchor
 onready var hook_shape := $Anchor/CollisionShape2D
 
-const HOOK_MAX_LENGTH = 600.0
+const HOOK_MAX_LENGTH = 250.0
 const HOOK_SPEED = 800.0
 const PLAYER_HOOK_SPEED = 600.0
 
@@ -23,7 +24,7 @@ func _physics_process(delta):
 	
 	match player_state:
 		PlayerStates.DEFAULT:
-			pass
+			hook.show()
 		PlayerStates.HOOKED:
 			var collision = get_colliding_bodies()
 			if not collision.empty():
@@ -33,25 +34,31 @@ func _physics_process(delta):
 				global_position = hook.global_position
 				player_state = PlayerStates.DEFAULT
 				hide_hook()
-			if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
-				player_state = PlayerStates.DEFAULT
-				hook_state = HookStates.RETRACT_TO_PLAYER
-			if Input.is_action_pressed("ui_up"):
-				player_state = PlayerStates.DEFAULT
-				hook_state = HookStates.RETRACT_TO_PLAYER
 	match hook_state:
 		HookStates.EXTEND:
-			hook.global_position += hook_direction * HOOK_SPEED * delta
+			var collision_info = hook.move_and_collide(hook_direction * HOOK_SPEED * delta)
+			if collision_info:
+				hook_collide()
 			if hook.global_position.distance_to(global_position) >= HOOK_MAX_LENGTH:
 				hook_state = HookStates.RETRACT_TO_PLAYER
 				hook_shape.call_deferred("set_disabled", true)
 		HookStates.RETRACT_TO_PLAYER:
-			hook.global_position += hook.global_position.direction_to(global_position) * HOOK_SPEED * delta
-			if hook.global_position.distance_to(global_position) <= HOOK_SPEED * delta:
+			var direction = hook.global_position.direction_to(global_position + Vector2(20, 20))
+			var collision_info = hook.move_and_collide(direction * HOOK_SPEED * delta)
+			if collision_info:
 				hook_state = HookStates.NONE
 				hide_hook()
 
-func _on_Anchor_body_entered(_body):
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
+		if hook_state == HookStates.NONE:
+			hook.global_position = global_position + Vector2(20, 20) # center it
+			hook_state = HookStates.EXTEND
+			hook_direction = Vector2(throw_speed, 0).rotated(rotation)
+			hook.show()
+			hook_shape.disabled = false
+
+func hook_collide():
 	player_state = PlayerStates.HOOKED
 	apply_impulse(Vector2.ZERO, global_position.direction_to(hook.global_position) * PLAYER_HOOK_SPEED)
 	hook_state = HookStates.NONE
